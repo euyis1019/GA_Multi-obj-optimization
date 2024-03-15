9b98bbfd89e162a8932413f7a4e46dcede25c2e5
euyis1019 <122277342+euyis1019@users.noreply.github.com>
2024-03-15 11:35:29 +0800

    Finish Framework

--- GA_insole/GP/Model2.py
+++ GA_insole/GP/Model2.py
@@ -1,104 +1,124 @@
-from deap import base, creator, tools, algorithms
-import random
-import numpy as np
-from evaluate_function import Buffer, Support, light_weight, fit, cal_area
+#!/usr/bin/env python
+# coding: utf-8
 
-# 参数范围
-param_bounds = {
-    'length': (0, 10),
-    'width': (0, 10),
-    'R': (0, 10),
-    'Thickness': (3,15),
-    'Hardness': (1, 70),
-    'A': (-10, 10),
-    'B': (-10, 10),
-    'C': (-10, 10),
-    'D': (-10, 10)
+import time
+import cv2
+import torch
+from skvideo.io import vreader, FFmpegWriter
+from ais_bench.infer.interface import InferSession
+
+from det_utils import letterbox, scale_coords, nms
+
+
+def preprocess_image(image, cfg, bgr2rgb=True):
+    img, scale_ratio, pad_size = letterbox(image, new_shape=cfg['input_shape'])
+    if bgr2rgb:
+        img = img[:, :, ::-1]
+    img = img.transpose(2, 0, 1)  # HWC2CHW
+    return img, scale_ratio, pad_size
+
+
+def draw_bbox(bbox, img0, color, wt, names):
+    det_result_str = ''
+    for idx, class_id in enumerate(bbox[:, 5]):
+        if float(bbox[idx][4] < float(0.05)):
+            continue
+        img0 = cv2.rectangle(img0, (int(bbox[idx][0]), int(bbox[idx][1])), (int(bbox[idx][2]), int(bbox[idx][3])),
+                             color, wt)
+        img0 = cv2.putText(img0, str(idx) + ' ' + names[int(class_id)], (int(bbox[idx][0]), int(bbox[idx][1] + 16)),
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        img0 = cv2.putText(img0, '{:.4f}'.format(bbox[idx][4]), (int(bbox[idx][0]), int(bbox[idx][1] + 32)),
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        det_result_str += '{} {} {} {} {} {}\n'.format(
+            names[bbox[idx][5]], str(bbox[idx][4]), bbox[idx][0], bbox[idx][1], bbox[idx][2], bbox[idx][3])
+    return img0
+
+
+def get_labels_from_txt(path):
+    labels_dict = dict()
+    with open(path) as f:
+        for cat_id, label in enumerate(f.readlines()):
+            labels_dict[cat_id] = label.strip()
+    return labels_dict
+
+
+def draw_prediction(pred, image, labels):
+    img_dw = draw_bbox(pred, image, (0, 255, 0), 2, labels)
+    cv2.imshow('result', img_dw)
+
+
+def infer_image(img_path, model, class_names, cfg):
+    image = cv2.imread(img_path)
+    img, scale_ratio, pad_size = preprocess_image(image, cfg)
+    output = model.infer([img])[0]
+
+    output = torch.tensor(output)
+    boxout = nms(output, conf_thres=cfg["conf_thres"], iou_thres=cfg["iou_thres"])
+    pred_all = boxout[0].numpy()
+    scale_coords(cfg['input_shape'], pred_all[:, :4], image.shape, ratio_pad=(scale_ratio, pad_size))
+    draw_prediction(pred_all, image, class_names)
+
+
+def infer_frame_with_vis(image, model, labels_dict, cfg, bgr2rgb=True):
+    img, scale_ratio, pad_size = preprocess_image(image, cfg, bgr2rgb)
+    output = model.infer([img])[0]
+
+    output = torch.tensor(output)
+    boxout = nms(output, conf_thres=cfg["conf_thres"], iou_thres=cfg["iou_thres"])
+    pred_all = boxout[0].numpy()
+    scale_coords(cfg['input_shape'], pred_all[:, :4], image.shape, ratio_pad=(scale_ratio, pad_size))
+    img_vis = draw_bbox(pred_all, image, (0, 255, 0), 2, labels_dict)
+    return img_vis
+
+
+def img2bytes(image):
+    return bytes(cv2.imencode('.jpg', image)[1])
+
+
+def infer_video(video_path, model, labels_dict, cfg, output_path='output.mp4'):
+    cap = vreader(video_path)
+    video_writer = None
+    for img_frame in cap:
+        image_pred = infer_frame_with_vis(img_frame, model, labels_dict, cfg, bgr2rgb=False)
+        cv2.imshow('result', image_pred)
+
+        if video_writer is None:
+            video_writer = FFmpegWriter(output_path)
+        video_writer.writeFrame(image_pred)
+    video_writer.close()
+
+
+def infer_camera(model, labels_dict, cfg):
+    cap = cv2.VideoCapture(0)
+    while True:
+        _, img_frame = cap.read()
+        infer_start = time.time()
+        image_pred = infer_frame_with_vis(img_frame, model, labels_dict, cfg)
+        infer_time = time.time() - infer_start
+        print(1 / infer_time)
+        cv2.imshow('result', image_pred)
+        cv2.waitKey(1)
+
+
+cfg = {
+    'conf_thres': 0.4,
+    'iou_thres': 0.5,
+    'input_shape': [640, 640],
 }
 
-
-# 评价函数
-def evalFit(individual, individual2):
-    # 从种群1的个体中获取A, B, C, D
-    A, B, C, D = individual[0], individual[1], individual[2], individual[3]
-
-    # 从种群2的个体中获取len和width
-    len, width = individual2[0], individual2[1]
-
-    # 调用fit函数
-    return (fit(len, width, A, B, C, D, 1, 1, 1, 1),)  # A, B, C, D
+model_path = 'om模型文件路径'
+label_path = '标签文件路径'
+model = InferSession(0, model_path)
+labels_dict = get_labels_from_txt(label_path)
 
 
-def evalSupport(individual):
-    return (Support(individual[0], individual[1], individual[2], 1, 1, 1, 1),)  # len, width, R
+infer_mode = 'camera'
 
-
-def evalBufferLightWeight(individual):
-    area = cal_area(1, 1, individual[0], individual[1], individual[2],
-                    individual[3])  # Assume fixed length and width for area calculation
-    return (
-    Buffer(individual[0], individual[1]) + light_weight(individual[0], area, individual[1]),)  # Thickness, Hardness
-
-
-# 设置GA基础结构
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
-
-toolbox = base.Toolbox()
-
-# 遗传算法参数
-POP_SIZE = 100
-GENS = 40
-CXPB, MUTPB = 0.5, 0.2
-
-# 遗传操作
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.2)
-toolbox.register("select", tools.selTournament, tournsize=3)
-
-# 统计操作
-stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", np.mean)
-stats.register("std", np.std)
-stats.register("min", np.min)
-stats.register("max", np.max)
-
-# 定义每个种群的结构和评价函数
-populations = {'surface': {'params': ['A', 'B', 'C', 'D'], 'eval': evalFit},
-               'arch': {'params': ['length', 'width', 'R'], 'eval': evalSupport},
-               'material': {'params': ['Thickness', 'Hardness'], 'eval': evalBufferLightWeight}}
-
-# 结果存储
-# 结果存储
-best_individuals = {}
-
-# 对每个种群运行GA
-for key, setting in populations.items():
-    # 初始化种群
-    for param in setting['params']:
-        toolbox.register(f"attr_{param}", random.uniform, *param_bounds[param])
-    toolbox.register("individual", tools.initCycle, creator.Individual,
-                     (toolbox.__getattribute__(f"attr_{param}") for param in setting['params']),
-                     n=1)
-    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-
-    # 初始化种群
-    pop = toolbox.population(n=POP_SIZE)
-
-    # 记录最佳个体
-    hof = tools.HallOfFame(1)
-
-    # 应用遗传算法
-    algorithms.eaSimple(pop, toolbox, CXPB, MUTPB, GENS, stats=stats, halloffame=hof, verbose=True)
-
-    # 记录最佳个体
-    best_individuals[key] = hof[0]
-
-# 输出每个种群的最佳个体
-for key, individual in best_individuals.items():
-    print(f"Best individual from {key} population: {individual}")
-    print(f"Fitness: {individual.fitness.values}\n")
-
-
-# 如果需要，这里可以将这些最佳个体组合在一起，以形成最终的解决方案
+if infer_mode == 'image':
+    img_path = 'world_cup.jpg'
+    infer_image(img_path, model, labels_dict, cfg)
+elif infer_mode == 'camera':
+    infer_camera(model, labels_dict, cfg)
+elif infer_mode == 'video':
+    video_path = 'world_cup.mp4'
+    infer_video(video_path, model, labels_dict, cfg, output_path='output.mp4')
